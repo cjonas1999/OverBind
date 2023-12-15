816532540e9c4a334c0bd061d36b35ff31cdda0b
cjonas1999 <cjonas1999@gmail.com>
2023-12-14 23:36:29 -0600

    Allow rebinding for entire virtual controller, and switch to json for config file

--- ui/src-tauri/src/key_interceptor.rs
+++ ui/src-tauri/src/key_interceptor.rs
@@ -1,6 +1,9 @@
 use once_cell::sync::Lazy;
+use serde::Deserialize;
+use std::cmp;
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{Read};
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
@@ -14,28 +17,33 @@ use windows::Win32::{
     },
 };
 
-static STICK_NEUTRAL: i16 = 0;
-static STICK_LEFT: i16 = -29000;
-static STICK_RIGHT: i16 = 29000;
-static STICK_UP: i16 = 29000;
-static STICK_DOWN: i16 = -29000;
-static KEYBIND_LEFT_STICK_LEFT: usize = 0;
-static KEYBIND_LEFT_STICK_RIGHT: usize = 1;
-static KEYBIND_RIGHT_STICK_UP: usize = 2;
+#[derive(Debug, Deserialize)]
+struct ConfigJsonData {
+    keycode: String,
+    result_type: String,
+    result_value: i32,
+}
 
-static KEYBINDS: Lazy<Arc<Mutex<Vec<u32>>>> = Lazy::new(|| Arc::new(Mutex::new(vec![])));
-static mut KEY_HELD: [bool; 3] = [false; 3];
+struct KeyState {
+    is_pressed: bool,
+    results: Vec<KeyResult>
+}
+
+struct KeyResult {
+    result_type: String,
+    result_value: i32
+}
+
+static KEY_STATES: Lazy<Arc<Mutex<HashMap<u32, KeyState>>>> = Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
 
 struct SharedState {
     target: Option<vigem_client::Xbox360Wired<Client>>,
-    gamepad: Option<vigem_client::XGamepad>,
     hook_handle: Option<HHOOK>,
 }
 
 static SHARED_STATE: Lazy<Arc<Mutex<SharedState>>> = Lazy::new(|| {
     Arc::new(Mutex::new(SharedState {
         target: None,
-        gamepad: None,
         hook_handle: None,
     }))
 });
@@ -63,34 +71,41 @@ impl KeyInterceptor {
         // Wait for the virtual controller to be ready to accept updates
         target.wait_ready().map_err(|e| e.to_string())?;
 
-        // Initialize the gamepad state
-        let gamepad = vigem_client::XGamepad {
-            ..Default::default()
-        };
 
         let mut shared_state = SHARED_STATE.lock().unwrap();
         shared_state.target = Some(target);
-        shared_state.gamepad = Some(gamepad);
 
         Ok(())
     }
 
     pub fn start(&mut self) -> Result<(), String> {
         // Read keybindings from file
-        let path = Path::new("OverBind_conf.txt");
-        let file = File::open(&path).map_err(|e| e.to_string())?;
-        let reader = BufReader::new(file);
-        let mut keybinds = Vec::new();
-        for line in reader.lines() {
-            let line = line.map_err(|e| e.to_string())?.trim().to_owned();
+        let path = Path::new("OverBind_conf.json");
+        let mut file = File::open(&path).map_err(|e| e.to_string())?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).map_err(|e| e.to_string())?;
+        
+        let data: Vec<ConfigJsonData> = serde_json::from_str(&contents).map_err(|e| e.to_string())?;
+        let mut key_states = HashMap::new();
 
-            if let Ok(value) = u32::from_str_radix(&line, 16) {
-                keybinds.push(value);
-            } else {
-                return Err(format!("Failed to parse keybinding: {}", line));
-            }
+        for item in data {
+            let keycode = u32::from_str_radix(&item.keycode, 16).expect("Invalid hexadecimal string");
+            let key_result = KeyResult {
+                result_type: item.result_type,
+                result_value: item.result_value
+            };
+
+            let key_state = key_states
+            .entry(keycode)
+            .or_insert_with(|| KeyState {
+                is_pressed: false,
+                results: Vec::new(),
+            });
+
+            key_state.results.push(key_result);
         }
-        *KEYBINDS.lock().unwrap() = keybinds;
+
+        *KEY_STATES.lock().unwrap() = key_states;
 
         self.should_run.store(true, Ordering::SeqCst);
 
@@ -125,15 +140,44 @@ impl KeyInterceptor {
     }
 }
 
+
+// Used for when multiple stick rebinds are set, first prioritizes right/up(positive) over left/down(negative), then larger values within those bands
+// neutral(zero) is the absolute lowest priority
+fn analog_priority_transform(n: i32) -> i32 {
+    if n < 0 {
+        return -1 * n
+    }
+    if n > 0 {
+        return n + i16::MAX as i32
+    }
+
+    0
+}
+
+fn find_higher_priority(num1: i16, num2: i16) -> i16 {
+    if num1 == 0 {
+        return num2
+    }
+    if num2 == 0{
+        return num1
+    }
+
+    if analog_priority_transform(num1 as i32) >= analog_priority_transform(num2 as i32) {
+        return num1
+    }
+    else {
+        return num2
+    }
+}
+
 unsafe extern "system" fn low_level_keyboard_proc_callback(
     n_code: i32,
     w_param: WPARAM,
     l_param: LPARAM,
 ) -> LRESULT {
-    let keybinds = KEYBINDS.lock().unwrap();
+    let mut key_states = KEY_STATES.lock().unwrap();
     let mut shared_state = SHARED_STATE.lock().unwrap();
     let mut temp_target = shared_state.target.take();
-    let mut temp_gamepad = shared_state.gamepad.take();
     let hook_handle = shared_state.hook_handle.unwrap();
 
     if n_code != HC_ACTION as i32 {
@@ -141,56 +185,64 @@ unsafe extern "system" fn low_level_keyboard_proc_callback(
     }
 
     let kbd_struct = l_param.0 as *const KBDLLHOOKSTRUCT;
-    println!("Key {:?} {:?}", w_param, (*kbd_struct).vkCode);
+    //println!("Key {:?} {:?}", w_param, (*kbd_struct).vkCode);
 
     match w_param.0 as u32 {
         WM_KEYUP | WM_SYSKEYUP => {
-            for i in 0..3 {
-                if (*kbd_struct).vkCode == keybinds[i] {
-                    KEY_HELD[i] = false;
-                }
+            match key_states.get_mut(&(*kbd_struct).vkCode) {
+                Some(state) => {state.is_pressed = false},
+                _ => ()
             }
         }
 
         WM_KEYDOWN | WM_SYSKEYDOWN => {
-            for i in 0..3 {
-                if (*kbd_struct).vkCode == keybinds[i] {
-                    KEY_HELD[i] = true;
-                }
+            match key_states.get_mut(&(*kbd_struct).vkCode) {
+                Some(state) => {state.is_pressed = true},
+                _ => ()
             }
         }
 
         _ => return CallNextHookEx(HHOOK::default(), n_code, w_param, l_param),
     }
 
-    let mut left_stick_x: i16 = STICK_NEUTRAL;
-    let mut left_stick_y: i16 = STICK_NEUTRAL;
-    let mut right_stick_x: i16 = STICK_NEUTRAL;
-    let mut right_stick_y: i16 = STICK_NEUTRAL;
 
-    if KEY_HELD[KEYBIND_LEFT_STICK_RIGHT] {
-        left_stick_x = STICK_RIGHT;
-    } else if KEY_HELD[KEYBIND_LEFT_STICK_LEFT] {
-        left_stick_x = STICK_LEFT;
-    } else {
-        left_stick_x = STICK_NEUTRAL;
+    let mut face_buttons: u16 = 0;
+    let mut left_trigger: u8 = 0;
+    let mut right_trigger: u8 = 0;
+    let mut thumb_lx: i16 = 0;
+    let mut thumb_ly: i16 = 0;
+    let mut thumb_rx: i16 = 0;
+    let mut thumb_ry: i16 = 0;
+
+    for (_, key_state) in key_states.iter().filter(|&(_, ks)| ks.is_pressed) {
+        for result in key_state.results.iter() {
+            match result.result_type.as_str() {
+                "face_button" => { face_buttons = face_buttons | result.result_value as u16 },
+                "left_trigger" => { left_trigger = cmp::max(left_trigger, result.result_value as u8) },
+                "right_trigger" => { right_trigger = cmp::max(right_trigger, result.result_value as u8) },
+                "thumb_lx" => { thumb_lx = find_higher_priority(thumb_lx, result.result_value as i16) },
+                "thumb_ly" => { thumb_ly = find_higher_priority(thumb_ly, result.result_value as i16) },
+                "thumb_rx" => { thumb_rx = find_higher_priority(thumb_rx, result.result_value as i16) },
+                "thumb_ry" => { thumb_ry = find_higher_priority(thumb_ry, result.result_value as i16) },
+                _ => ()
+            }
+        }
     }
 
-    if KEY_HELD[KEYBIND_RIGHT_STICK_UP] {
-        right_stick_y = STICK_UP;
-    } else {
-        right_stick_y = STICK_NEUTRAL;
-    }
+    let gamepad = vigem_client::XGamepad {
+        buttons: vigem_client::XButtons(face_buttons),
+        left_trigger: left_trigger,
+        right_trigger: right_trigger,
+        thumb_lx: thumb_lx,
+        thumb_ly: thumb_ly,
+        thumb_rx: thumb_rx,
+        thumb_ry: thumb_ry
+    };
 
-    if let (Some(ref mut target), Some(ref mut gamepad)) = (&mut temp_target, &mut temp_gamepad) {
-        gamepad.thumb_lx = left_stick_x;
-        gamepad.thumb_ly = left_stick_y;
-        gamepad.thumb_rx = right_stick_x;
-        gamepad.thumb_ry = right_stick_y;
+    if let Some(ref mut target) = &mut temp_target {
         let _ = target.update(&gamepad);
 
         shared_state.target = temp_target;
-        shared_state.gamepad = temp_gamepad;
     }
 
     return CallNextHookEx(hook_handle, n_code, w_param, l_param);
