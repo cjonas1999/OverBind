d0cfdd392590097cc5a88d0458a8bae7972eda65
arnabp <arnab.purkayastha@colorado.edu>
2025-12-04 21:23:53 -0500

    Update pointer chain to one that persists over saves

--- src-tauri/src/text_masher.rs
+++ src-tauri/src/text_masher.rs
@@ -3,17 +3,21 @@ use asr::{
     Address, PointerSize, Process,
 };
 use once_cell::sync::Lazy;
-use std::{sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-}, time::Instant};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
 use std::{thread::sleep, time::Duration};
 
 pub const MAX_MASHING_KEY_COUNT: u8 = 3;
 
 const TARGET_RATE: f64 = 37.0;
 pub static IS_MASHER_ACTIVE: Lazy<Arc<AtomicBool>> = Lazy::new(|| Arc::new(AtomicBool::new(false)));
-pub static SHOULD_TERMINATE_MASHER: Lazy<Arc<AtomicBool>> = Lazy::new(|| Arc::new(AtomicBool::new(false)));
+pub static SHOULD_TERMINATE_MASHER: Lazy<Arc<AtomicBool>> =
+    Lazy::new(|| Arc::new(AtomicBool::new(false)));
 
 struct HKConfig {
     module_name: &'static str,
@@ -26,8 +30,8 @@ static CONFIGS: &[(&str, HKConfig)] = &[
         "hollow_knight.x86_64",
         HKConfig {
             module_name: "libmono.so",
-            base_offset: 0x004AAA68,
-            pointer_chain: &[0x138, 0x898, 0x20, 0x28, 0x10c],
+            base_offset: 0x004ABF88,
+            pointer_chain: &[0xa0, 0x268, 0x8, 0x10, 0x70, 0x20, 0x10c],
         },
     ),
     (
@@ -122,7 +126,10 @@ fn resolve_pointer_chain(
     Some(addr + chain[chain.len() - 1])
 }
 
-pub fn text_masher(do_key_event: impl Fn(u8), toggle_overlay: impl Fn(bool) -> Result<(), Box<dyn std::error::Error>>) {
+pub fn text_masher(
+    do_key_event: impl Fn(u8),
+    toggle_overlay: impl Fn(bool) -> Result<(), Box<dyn std::error::Error>>,
+) {
     log::info!("TextMasher starting up");
     let target_interval: Duration = Duration::from_secs_f64(1.0 / TARGET_RATE);
     IS_MASHER_ACTIVE.store(false, Ordering::SeqCst);
@@ -131,7 +138,7 @@ pub fn text_masher(do_key_event: impl Fn(u8), toggle_overlay: impl Fn(bool) -> R
     'mainloop: loop {
         if SHOULD_TERMINATE_MASHER.load(Ordering::SeqCst) {
             let _ = toggle_overlay(false);
-            break 'mainloop
+            break 'mainloop;
         }
 
         log::info!("GameManagerFinder wait_attach...");
@@ -145,7 +152,7 @@ pub fn text_masher(do_key_event: impl Fn(u8), toggle_overlay: impl Fn(bool) -> R
                     log::info!("Breaking mainloop in masher thread");
                     return;
                 }
-                
+
                 if IS_MASHER_ACTIVE.load(Ordering::SeqCst) {
                     if let Some(err) = toggle_overlay(false).err() {
                         log::error!("Failed to toggle masher overlay: {}", err);
@@ -158,34 +165,38 @@ pub fn text_masher(do_key_event: impl Fn(u8), toggle_overlay: impl Fn(bool) -> R
 
                     let base: Address = module_address + config.base_offset;
                     let dialogue_box_opt = resolve_pointer_chain(
-                                    &process,
-                                    base,
-                                    &config.pointer_chain,
-                                    PointerSize::Bit64,
-                                );
-                                
-                                let input_pointer: UnityPointer<3> = UnityPointer::new(
-                                    "GameManager",
+                        &process,
+                        base,
+                        &config.pointer_chain,
+                        PointerSize::Bit64,
+                    );
+
+                    let input_pointer: UnityPointer<3> = UnityPointer::new(
+                        "GameManager",
                         0,
                         &[
                             "_instance",
                             "<inputHandler>k__BackingField",
                             "acceptingInput",
-                            ],
-                        );
+                        ],
+                    );
 
                     let accepting_input: bool = input_pointer
-                    .deref(&process, &module, &image)
-                    .unwrap_or_default();
+                        .deref(&process, &module, &image)
+                        .unwrap_or_default();
 
                     if accepting_input {
                         if let Some(dialogue_box_addr) = dialogue_box_opt {
                             let mut next_time = Instant::now() + target_interval;
                             let mut key_to_press = 0;
 
-                            if IS_MASHER_ACTIVE.load(Ordering::SeqCst) && matches!(process.read::<u8>(dialogue_box_addr + 0x2E), Ok(is_dialogue_hidden) if is_dialogue_hidden == 0) {
-                                do_key_event(100);// release keys
-                                while IS_MASHER_ACTIVE.load(Ordering::SeqCst) && matches!(process.read::<u8>(dialogue_box_addr + 0x2E), Ok(is_dialogue_hidden) if is_dialogue_hidden == 0) {
+                            if IS_MASHER_ACTIVE.load(Ordering::SeqCst)
+                                && matches!(process.read::<u8>(dialogue_box_addr + 0x2E), Ok(is_dialogue_hidden) if is_dialogue_hidden == 0)
+                            {
+                                do_key_event(100); // release keys
+                                while IS_MASHER_ACTIVE.load(Ordering::SeqCst)
+                                    && matches!(process.read::<u8>(dialogue_box_addr + 0x2E), Ok(is_dialogue_hidden) if is_dialogue_hidden == 0)
+                                {
                                     let _ = toggle_overlay(true);
                                     log::debug!("Trigger do key event: {}", key_to_press);
                                     do_key_event(key_to_press);
